612d370c6689a907361709ec423621d72bd99bc1
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2020-04-30 23:01:29 -0600

    added search controller

--- JS-recipe-app/src/js/index.js
+++ JS-recipe-app/src/js/index.js
@@ -1,15 +1,31 @@
-import axios from 'axios';
 import '../styles/styles.css';
-import * as search from './models/Search';
+import Search from './models/Search';
 
+/* Global state of the app
+-search object
+-current recipe object
+-shopping list object
+-liked recipes
+*/
+const state = {};
+const controlSearch = async () => {
+    // 1. get query fromview
+    const query = 'pizza';
 
-async function getResults(query){
-    try{
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`)
-        const recipes = result.data.recipes;
-        console.log(recipes)
-    }catch(error){
-        alert(error)
+    if(query){
+        //2. New search object and add to state
+        state.search = new Search(query);
+        //3. Prepare UI for resault (spinner)
+
+        //4. Search for recipes
+        await state.search.getResults();
+        // 5. Render resaults on UI
+        console.log(state.search.recipes);
+
     }
+
 }
-getResults('pasta');
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
